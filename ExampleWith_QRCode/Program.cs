784eaa22052e6160a5fdbb7fc2095ded6a42ce85
Rüfət İsmayılov <rufet.ismayilov.1999@outlook.com>
2020-03-05 00:21:23 +0400

    Add project files.

---
+++ ExampleWith_QRCode/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Spire;
+using Spire.Barcode;
+
+namespace ExampleWith_QRCode
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+          
+            BarcodeSettings settings = new BarcodeSettings();
+            settings.Type = BarCodeType.Aztec;
+            settings.Unit = GraphicsUnit.Inch;
+            settings.ShowText = false;
+            settings.AutoResize = true;
+
+            settings.ResolutionType = ResolutionType.Printer;
+            settings.Data = "https://github.com/ismayilov449";
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+            Image QRbarcode = generator.GenerateImage();
+            
+
+            QRbarcode.Save(@"C:\Users\Rufat\source\repos\ExampleWith_QRCode\ExampleWith_QRCode\Images\"+ Guid.NewGuid().ToString() + ".jpeg", ImageFormat.Jpeg);
+             
+
+        }
+
+
+    }
+}
